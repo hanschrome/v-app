42953358270b9c2c0ec417e488afe7967741e848
Hans Castro <hanscastroadmn@gmail.com>
2022-05-04 22:50:56 +0200

    [ALIAS] Added aliases .sh file

---
+++ aliases.sh
@@ -0,0 +1,6 @@
+# v-app alias
+
+alias firefox='cd ~/v-app && ./v-app firefox && exit'
+alias google-chrome='cd ~/v-app && ./v-app google-chrome && exit'
+alias gimp='cd ~/v-app && ./v-app gimp'
+alias inkscape='cd ~/v-app && ./v-app inkscape'
