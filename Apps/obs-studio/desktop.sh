d93f38f710030e622008c92d2fc2ed696b2a637a
Hans <hanscastroadmn@gmail.com>
2022-06-25 18:52:14 +0200

    Update desktop.sh

--- Apps/obs-studio/desktop.sh
+++ Apps/obs-studio/desktop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ffmpeg -f x11grab -r 15 -s 1920x1080 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video2
-
+ffmpeg -f x11grab -r 30 -probesize 42M -s 1920x1080 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video2
